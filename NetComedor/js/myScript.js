71e0dbdb83ac462b3b25d88c5916c243de822b4c
RubenDiazRuiz <nebur_diaz@hotmail.com>
2018-05-10 17:31:08 +0200

    subo js

--- NetComedor/js/myScript.js
+++ NetComedor/js/myScript.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   // FUNCION PARA QUE ESCRIBA EL CONTENIDO DEL INPUT CANTIDAD EN EL CAMPO CANTIDAD (FACTURA)
+  //AL LEVANTAR LA TECLA
   $("#cantidadTicket").change(function(){
     var numticket = $(this).val();
     $("#textoCant").html(numticket);
   });
-
+  // AL CAMBIAR DE NUMERO CON LAS FLECHAS DEL INPUT
   $("#cantidadTicket").keyup(function(){
     var numticket = $(this).val();
     $("#textoCant").html(numticket);
@@ -15,6 +16,7 @@ $(document).ready(function(){
     var str = "";
     $( "select option:selected" ).each(function() {
       str += $( this ).text() + " ";
+      $
     });
     $( "#txtTicket" ).html( str );
   })
