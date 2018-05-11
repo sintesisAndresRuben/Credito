0f2f51ea02a84c5a44c81d453a164f39205eac99
Andres <andres.gonzalez.pradas@gmail.com>
2018-05-11 15:50:56 +0200

    bd

--- NetComedor/normativaSubida.proc.php
+++ NetComedor/normativaSubida.proc.php
@@ -8,14 +8,14 @@ if(!isset($_SESSION['user'])) {
 	// echo "Nombre: $pdf </br>";
 	// echo "Ruta:./normativa_pdf/$pdf </br>";
 	// echo "Fecha: $fecha </br>";
-	// echo "Etapa: </br>";
+	// echo "Etapa: 16 </br>";
+
+	//$q = "INSERT INTO tbl_normativa (nombre_normativa, ruta_normativa, fecha_normativa, id_etapa) VALUES ('$pdf','./normativa_pdf/$pdf', '$fecha', 16)";
+	//$subirPdf=mysqli_query($conexion, $q);
 	// Mover los ficheros al directorio que nosotros queremos
 	//http://www.holamundo.es/lenguaje/php/articulos/subir-archivos-con-php.html
-	move_uploaded_file($_FILES['archivo']['tmp_name'],'./normativa_pdf/'.$FILES['foto']['name'])
-	// 
-	$q = "INSERT INTO tbl_normativa (id_normativa, nombre_normativa, tipo_ruta, ruta_normativa, fecha_normativa, id_etapa) VALUES ('', $pdf, '','./normativa_pdf/$pdf', '$fecha', '16')";
-	$subirPdf=mysqli_query($conexion, $q);
-	echo $q;
-	//header("location:normativa.php");
+	move_uploaded_file($_FILES['archivo']['temp_name'],'./normativa_pdf/'.$FILES['foto']['name']);
+	chmod('./normativa_pdf/'.$FILES['foto']['name'],0644);
+	// header("location:normativa.php");
 	}
 ?>
